6005c0cf919730dffd20b1eb94023e5bc11b2701
Adam Deeley <ad.deeley@gmail.com>
2018-05-18 18:49:49 +0100

    Extracted setRotation.

--- js/src/index.js
+++ js/src/index.js
@@ -1,31 +1,34 @@
 const tiles = document.querySelectorAll('.tiles__tile');
-const shuffleButton = document.querySelector('#controls__shuffle');
-const NewPuzzleButton = document.querySelector('#controls__new-puzzle');
+
+function setRotation(tile, rotation) {
+    tile.style.setProperty('transform', `rotate(${rotation}deg)`);
+}
 
 function rotateTile(tile) {
     const currentRotation = /\d+/.exec(tile.style.transform);
     let newRotation = (currentRotation) ? parseInt(currentRotation) + 90 : 90;
     if (newRotation >= 360) newRotation = 0;
-    tile.style.setProperty('transform', `rotate(${newRotation}deg)`);
+    setRotation(tile, newRotation);
 }
 
 function shuffle() {
-    tiles.forEach((tile) => {
+    tiles.forEach(tile => {
         const newRotation = Math.floor(Math.random() * 4) * 90;
-        tile.style.setProperty('transform', `rotate(${newRotation}deg)`);
-    })
+        setRotation(tile, newRotation);
+    });
 }
 
 function positionTilesCorrectly() {
-    let size = 100;
-    let row = 0;
-    let col = 0;
+    const tileSize = 100;
+    const puzzleSize = 500;
+    let xOffset = 0;
+    let yOffset = 0;
     tiles.forEach(tile => {
-        tile.style.setProperty('background-position', `-${row}px -${col}px`);
-        row += size;
-        if (row >= 500) {
-            row = 0;
-            col += size;
+        tile.style.setProperty('background-position', `-${xOffset}px -${yOffset}px`);
+        xOffset += tileSize;
+        if (xOffset >= puzzleSize) {
+            xOffset = 0;
+            yOffset += tileSize;
         }
     });
 }
@@ -35,9 +38,13 @@ function newPuzzle() {
 }
 
 function init() {
-    tiles.forEach(tile => tile.addEventListener('click', (e) => rotateTile(e.target)));
+    const shuffleButton = document.querySelector('#controls__shuffle');
+    const NewPuzzleButton = document.querySelector('#controls__new-puzzle');
+
+    tiles.forEach(tile => tile.addEventListener('click', e => rotateTile(e.target)));
     shuffleButton.addEventListener('click', shuffle);
     NewPuzzleButton.addEventListener('click', newPuzzle);
+
     positionTilesCorrectly();
     shuffle();
 }
