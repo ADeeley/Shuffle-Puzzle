031067d8adc2c7202c82031567136a9f586a21be
Adam Deeley <ad.deeley@gmail.com>
2018-05-18 15:48:57 +0100

    Use setProperty to work with element css style object

--- js/src/index.js
+++ js/src/index.js
@@ -5,13 +5,13 @@ function rotateTile(tile) {
     const currentRotation = /\d+/.exec(tile.style.transform);
     let newRotation = (currentRotation) ? parseInt(currentRotation) + 90 : 90;
     if (newRotation >= 360) newRotation = 0;
-    tile.setAttribute('style', `transform: rotate(${newRotation}deg)`);
+    tile.style.setProperty('transform', `rotate(${newRotation}deg)`);
 }
 
 function shuffle() {
     tiles.forEach((tile) => {
         const newRotation = Math.floor(Math.random() * 4) * 90;
-        tile.setAttribute('style', `transform: rotate(${newRotation}deg)`);
+        tile.style.setProperty('transform', `rotate(${newRotation}deg)`);
     })
 }
 
